940ac2808d2077901c6a08c365bc30a878fb5de1
AlokSachan <alok.sachan.83@gmail.com>
2022-11-18 17:16:14 +0530

    initial commit for spring cloud gateway

---
+++ src/main/java/com/alok/gatewayserver/filters/TraceFilter.java
@@ -0,0 +1,45 @@
+package com.alok.gatewayserver.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Order(1)
+@Slf4j
+public class TraceFilter implements GlobalFilter {
+
+    @Autowired
+    FilterUtility filterUtility;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        HttpHeaders headers = exchange.getRequest().getHeaders();
+        if(isCorrelationIdPresent(headers)){
+            log.debug("EazyBank-correlation-id found in tracing filter: {}. ", filterUtility.getCorrelationId(headers));
+        }
+        else {
+            String correlationID = generateCorrelationId();
+            exchange = filterUtility.setCorrelationId(exchange, correlationID);
+            log.debug("EazyBank-correlation-id generated in tracing filter: {}.", correlationID);
+        }
+        return chain.filter(exchange);
+    }
+
+    private boolean isCorrelationIdPresent(HttpHeaders headers) {
+        if(filterUtility.getCorrelationId(headers) != null){
+            log.info("CORRELATION_ID : {}", filterUtility.getCorrelationId(headers));
+            return true;}
+        return false;
+    }
+
+    private String generateCorrelationId() {
+        return java.util.UUID.randomUUID().toString();
+    }
+}
