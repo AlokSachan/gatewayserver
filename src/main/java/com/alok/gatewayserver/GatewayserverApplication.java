eba07c9dfc53d2cf48d7fc19b173c636876e81fc
AlokSachan <alok.sachan.83@gmail.com>
2024-11-10 16:32:49 +0530

    use circuit breaker for resiliency

--- src/main/java/com/alok/gatewayserver/GatewayserverApplication.java
+++ src/main/java/com/alok/gatewayserver/GatewayserverApplication.java
@@ -1,40 +1,65 @@
 package com.alok.gatewayserver;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+
+import static org.springframework.cloud.gateway.support.RouteMetadataUtils.CONNECT_TIMEOUT_ATTR;
+import static org.springframework.cloud.gateway.support.RouteMetadataUtils.RESPONSE_TIMEOUT_ATTR;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
 
-	@Bean
-	public RouteLocator abcBankRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
-		return routeLocatorBuilder.routes()
-				.route(p->p
-						.path("/abc/accounts/**")
-						.filters(f -> f.rewritePath("/abc/accounts/(?<segments>.*)", "/${segments}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-						.uri("lb://ACCOUNTS"))
-				.route(p->p
-						.path("/abc/loans/**")
-						.filters(f -> f.rewritePath("/abc/loans/(?<segments>.*)", "/${segments}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-						.uri("lb://LOANS"))
-				.route(p->p
-						.path("/abc/cards/**")
-						.filters(f -> f.rewritePath("/abc/cards/(?<segments>.*)", "/${segments}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
-						.uri("lb://CARDS")).build();
-	}
+    @Bean
+    public RouteLocator abcBankRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p
+                        .path("/abc/accounts/**")
+                        .filters(f -> f.rewritePath("/abc/accounts/(?<segments>.*)", "/${segments}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .circuitBreaker(config -> config.setName("accountsCircuitBreaker")
+                                        .setFallbackUri("forward:/contactSupport")))
+//								.metadata(RESPONSE_TIMEOUT_ATTR,2000)
+//						.metadata(CONNECT_TIMEOUT_ATTR, 2000))
+
+                        .uri("lb://ACCOUNTS"))
+                .route(p -> p
+                        .path("/abc/loans/**")
+                        .filters(f -> f.rewritePath("/abc/loans/(?<segments>.*)", "/${segments}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                .retry(retryConfig -> retryConfig.setRetries(3)
+                                        .setMethods(HttpMethod.GET)
+                                        .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
+                        .uri("lb://LOANS"))
+                .route(p -> p
+                        .path("/abc/cards/**")
+                        .filters(f -> f.rewritePath("/abc/cards/(?<segments>.*)", "/${segments}")
+                                .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                        .uri("lb://CARDS")).build();
+    }
+// create bean to increase circuit breaker time out
+    @Bean
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
+    }
 
 }
